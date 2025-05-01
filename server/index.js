3c91662956f91aa45ea6524e156afd06c6d4a25d
Aniket Dey <meaniket1025@gmail.com>
2025-05-01 13:24:56 +0530

    url done

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const http = require("http");
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
+    if(req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()} - ${req.url} - req from client\n`
+    const myUrl = url.parse(req.url, true);
+    // console.log(myUrl);
     fs.appendFile('log.txt', log, (err, date) => {
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/':
                 res.end("This is home");
                 break;
             case '/about':
-                res.end("this in about");
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                break;
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end("here are your resuls for - " + search)
                 break;
             default:
                 res.end("404")
