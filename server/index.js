b3aef7f3952b0aeced8e5c2fe463f7ff7121651f
Aniket Dey <meaniket1025@gmail.com>
2025-05-01 18:33:58 +0530

    installed express

--- server/index.js
+++ server/index.js
@@ -1,32 +1,24 @@
 const http = require("http");
-const fs = require("fs")
-const url = require("url")
+// const fs = require("fs")
+// const url = require("url")
+const express = require("express")
 
-const myServer = http.createServer((req, res) => {
-    if(req.url === '/favicon.ico') return res.end();
-    const log = `${Date.now()} - ${req.method} - ${req.url} - req from client\n`
-    const myUrl = url.parse(req.url, true);
-    // console.log(myUrl);
-    fs.appendFile('log.txt', log, (err, date) => {
-        switch(myUrl.pathname){
-            case '/':
-                res.end("This is home");
-                break;
-            case '/about':
-                const username = myUrl.query.myname;
-                res.end(`Hi, ${username}`);
-                break;
-            case '/search':
-                const search = myUrl.query.search_query;
-                res.end("here are your resuls for - " + search)
-                break;
-            default:
-                res.end("404")
+const app = express();
 
-        }
-    })
-});
+app.get("/", (req, res) => {
+    return res.send("this is home");
+})
 
-myServer.listen(8000, () =>{
+app.get("/about", (req, res) => {
+    return res.send("this is about, hi " + req.query.name)
+})
+
+app.listen(8000, () => {
     console.log("server started");
-});
+})
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () =>{
+//     console.log("server started");
+// });
