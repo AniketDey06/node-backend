2f6878a3deffa4ae69b81754e5106ab37b93faa5
Aniket Dey <meaniket1025@gmail.com>
2025-05-04 00:25:34 +0530

    misc

--- restAPI-1/index.js
+++ restAPI-1/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const users = require("./MOCK_DATA.json");
-const { log } = require("console");
 
 const app = express();
 const PORT = 8000;
