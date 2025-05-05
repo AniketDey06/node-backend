4c47865607d2c273c160bccc49e6fa3445fd0a67
Aniket Dey <meaniket1025@gmail.com>
2025-05-05 16:02:24 +0530

    implemented MVC pattern

--- restAPI-1/index.js
+++ restAPI-1/index.js
@@ -1,133 +1,24 @@
-const { error } = require("console");
 const express = require("express");
-const fs = require("fs");
-const mongoose = require("mongoose"); 
 require('dotenv').config();
 
-// const users = require("./MOCK_DATA.json");
+const { connectMongoDb } = require('./utils/db')
+const { logReqRes } = require('./middlewares/index.middlewares')
+const userRouter = require('./routes/user.routes')
 
-// db con
-mongoose
-    .connect(process.env.db_URI)
-    .then(() => console.log("DB connect"))
-    .catch(err => console.log(err))
-
-
-
+// ports setup
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Schema
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    jobTitle: {
-        type: String,
-    },
-    gender: {
-        type: String,
-    }
-},{timestamps: true})
+// db con
+connectMongoDb(process.env.db_URI)
 
-const User = mongoose.model("user", userSchema);
+// Schema
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
-
-app.use((req, res, next) => {
-    fs.appendFile('log.txt', 
-        `\n${Date.now()} - ${req.ip} - ${req.method} - ${req.path}`,
-        (err, data) => {
-            if(err){
-                console.log("err: ",err);
-            }
-            next();
-        }
-    );
-
-    // return res.json({msg: "hello from Middleware1"})
-})
-
-
+app.use(logReqRes("log.txt"))
 
 // Routes 
-app.get('/', (req, res) => {
-    return res.send("this is home")
-})
-
-app.get('/users', async (req, res) => {
-    const allDbUsers = await User.find({});
-    const html = 
-    `
-        <ul>
-            ${allDbUsers.map((user) => `<li>ID - ${user._id} - ${user.firstName} - ${user.email}</li></br>`).join("")}
-        </ul>
-    `
-    return res.send(html)
-})
-
-app.get('/api/users', async (req, res) => {
-    const allDbUsers = await User.find({});
-    return res.json(allDbUsers)
-})
-
-app.route('/api/users/:id')
-    .get(async(req, res) => {
-        const user = await User.findById(req.params.id);
-        if(!user) return res.status(400).json({error: "User not Found"});
-        return res.json(user);
-    })
-    .patch( async (req, res) => {
-        // const id = Number(req.params.id);
-        const body = req.body;
-
-        // const idx = users.findIndex((user) => user.id === id);
-        
-        await User.findByIdAndUpdate(req.params.id, { ...body})
-        .then(console.log())
-        .catch((err) => console.log(err))
-
-        return res.json({ success: true});
-        
-    })
-    .delete( async (req, res) => {
-        await User.findByIdAndDelete(req.params.id);
-        return res.json({ success: true});
-    })
-
-app.post("/api/users", async(req, res) => {
-    const body = req.body;
-    if (
-        !body ||
-        !body.first_name ||
-        !body.last_name ||
-        !body.email ||
-        !body.gender ||
-        !body.job_title
-    ) {
-        return res.status(400).json({msg: "all fields are req.. "})
-    }
-
-    const result = await User.create({
-        firstName: body.first_name,
-        lastName: body.last_name,
-        email: body.email,
-        gender: body.gender,
-        jobTitle: body.job_title
-    })
-
-    return res.status(201).json({msg: "success"});
-
-})
+app.use("/api/users", userRouter)
 
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`))
---
+++ restAPI-1/middlewares/index.middlewares.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+function logReqRes(filename){
+    return (req, res, next) => {
+        fs.appendFile(filename, 
+            `\n${Date.now()} - ${req.ip} - ${req.method} - ${req.path}`,
+            (err, data) => {
+                if(err){
+                    console.log("err: ",err);
+                }
+                next();
+            }
+        );
+    }
+}
+
+module.exports = {
+    logReqRes,
+}
