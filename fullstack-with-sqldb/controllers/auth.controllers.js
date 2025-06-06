8e68b3fa271e9ba528ab7c81171458a87a73f1a9
Aniket Dey <meaniket1025@gmail.com>
2025-06-06 20:55:51 +0530

    forgot password done

--- fullstack-with-sqldb/controllers/auth.controllers.js
+++ fullstack-with-sqldb/controllers/auth.controllers.js
@@ -230,7 +230,7 @@ const logoutUser = async (req, res) => {
                 id: req.user.id,
             }
         })
-        res.cookie(`token`, "",{})
+        res.cookie(`token`, "", {})
         return res.status(200).json({
             success: true,
             user,
@@ -245,10 +245,53 @@ const logoutUser = async (req, res) => {
     }
 }
 
+const forgotPassword = async (req, res) => {
+    const { email } = req.body
+    console.log(email );
+
+    if (!email) {
+        return res.status(400).json({
+            success: false,
+            message: "Email field is required",
+        });
+    }
+
+    try {
+        const restToken = crypto.randomBytes(32).toString('hex');
+        console.log(`in try`, restToken);
+        const restTokenExpiry = Date.now() + (10 * 60 * 1000)
+        console.log(`in try`, restTokenExpiry);
+
+        const user = await prisma.user.update({
+            where: {
+                email
+            },
+            data: {
+                passwordRestToken: restToken,
+                passwordRestExpiry: restTokenExpiry.toString(),
+            }
+        })
+
+
+        return res.status(200).json({
+            success: true,
+            user,
+            message: "Reset token generated and saved",
+        })
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            error,
+            message: "forgotPassword faild",
+        })
+    }
+}
+
 export {
     registerUser,
     loginUser,
     verifyUser,
     getProfile,
     logoutUser,
+    forgotPassword,
 }
--- fullstack-with-sqldb/routes/auth.route.js
+++ fullstack-with-sqldb/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, verifyUser, loginUser, getProfile, logoutUser } from '../controllers/auth.controllers.js'
+import { registerUser, verifyUser, loginUser, getProfile, logoutUser, forgotPassword } from '../controllers/auth.controllers.js'
 import { isLogedIn } from '../middleware/auth.middleware.js'
 const userRouter = express.Router()
 
@@ -8,5 +8,8 @@ userRouter.post('/login', loginUser);
 userRouter.get('/verify/:token', verifyUser);
 userRouter.get('/profile', isLogedIn, getProfile)
 userRouter.get('/logout', isLogedIn, logoutUser)
+userRouter.post("/forgotpassword", forgotPassword)
+// router.post("/resetpassword/:token", resetPassword)
+
 
 export default userRouter;
