b7cfabba085a4e0110b9abb6a2f7a3cf4a4c880a
Aniket Dey <meaniket1025@gmail.com>
2025-06-09 21:53:54 +0530

    new blog project setup

--- blog-app/src/models/User.model.js
+++ blog-app/src/models/User.model.js
@@ -1,3 +1,34 @@
-import mongoose from "mongoose";
- 
-const user = new mongoose.Schema
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unquie: true,
+    },
+    salt: {
+        type: String,
+        require: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: '/images/default.png',
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    },
+}, { timestamps: true })
+
+const User = model('user', userSchema); 
+
+module.exports = User;
