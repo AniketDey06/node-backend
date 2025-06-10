ed208e9cdc5ba34e3590f4ef5d8d6ae1449d0827
Aniket Dey <meaniket1025@gmail.com>
2025-06-10 17:00:42 +0530

    jwt done

---
+++ blog-app/src/services/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+function creatUserToke (user) {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        profileImageURL : user.profileImageURL,
+        role: user.role 
+    }
+
+    const token = jwt.sign(payload, process.env.SECRET_KEY)
+    return token
+}
+
+function validateUserToke (token) {
+    const payload = jwt.verify(token, process.env.SECRET_KEY)
+    return payload;
+}
+
+export{
+    creatUserToke,
+    validateUserToke
+}
