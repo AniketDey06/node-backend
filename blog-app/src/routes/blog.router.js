aad572ed81dd783563f66c9be38cb97ded86f814
Aniket Dey <meaniket1025@gmail.com>
2025-06-11 01:01:55 +0530

    added: upload

--- blog-app/src/routes/blog.router.js
+++ blog-app/src/routes/blog.router.js
@@ -1,10 +1,29 @@
 import { Router } from 'express'
+import multer from 'multer'
+import path from 'path'
 
 import { addNewBlogRender } from '../controllers/blog.controller.js'
 
 const blogRouter = Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve('./public/uploads/'));
+    },
+    filename: function (req, file, cb) {
+        const filename = `${Date.now()}-${file.originalname}`
+        cb(null, filename)
+    },
+})
+
+const upload = multer({ storage: storage })
+
 blogRouter.get('/add-new', addNewBlogRender)
+blogRouter.post('/', upload.single('coverImage'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    return res.redirect("/");
+})
 
 export {
     blogRouter
