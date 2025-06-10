5c0fe6b750b468d25261f1a3b91ea9d95a000ee7
Aniket Dey <meaniket1025@gmail.com>
2025-06-10 17:01:12 +0530

    setup jwt cookies

--- blog-app/src/controllers/user.controller.js
+++ blog-app/src/controllers/user.controller.js
@@ -23,12 +23,16 @@ const signup = async (req, res) => {
 
 const signin = async (req, res) => {
     const { email, password } = req.body
-
-    const user = await User.matchPassword(email, password)
-
-    console.log('user', user);
-
-    return res.redirect("/");
+    try {
+        const token = await User.matchPassword(email, password)
+    
+        console.log('token', token);
+        return res.cookie("token", token).redirect("/");
+    } catch (error) {
+        return res.render("signin",{
+            error: 'Incorrect Email or Password'
+        });
+    }
 }
 
 export {
