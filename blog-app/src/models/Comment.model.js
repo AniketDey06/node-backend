8c0d25bb5f39f40ac61d241a356de7bbf3bea2d2
Aniket Dey <meaniket1025@gmail.com>
2025-06-11 13:11:27 +0530

    added comment schema

---
+++ blog-app/src/models/Comment.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        require: true,
+    },
+    blogId: {
+        type: Schema.Types.ObjectId,
+        ref: "blog"
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+    },
+},{timestamps: true});
+
+const Comment = mongoose.model('comment', commentSchema);
+
+export default Comment
